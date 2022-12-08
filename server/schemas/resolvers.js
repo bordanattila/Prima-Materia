a409c392c7b7b5715a24ace270e2e819c8c12ed9
Talee Y <talee.yng@gmail.com>
2022-12-07 21:10:09 -0600

    Add query for 'me'. Add Mutations for createUser, login, and addCardToWishlist.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,66 @@
-// Queries:
-// me
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
 
-// Mutations:
+const resolvers = {
+    // Queries:
+    Query: {
+    // me
+      me: async (parent, args, context) => {
+        // check if users exist
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select(
+            "-__v -password"
+          );
+          return userData;
+        }
+        throw new AuthenticationError("You need to be logged in!");
+      },
+    },
 
-// createUser
+    // Mutations:
+    Mutation: {
+    // createUser
+        createUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
-// login logout
+            return { token, user };
+        },
+    // login logout
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({ email });
+            if(!user) {
+                throw new AuthenticationError("No user with this email found!");
+            }
+            const correctPassword = await user.isCorrectPassword(password);
 
-// addCardToWishlist
+            if (!correctPassword) {
+                throw new AuthenticationError("Incorrect password")
+            }
 
-// addCardToDeck
+            const token = signToken(user);
+            return { token, user };
+        },
+    // TODO: addCardToWishlist
+        addCardToWishlist: async (parent, {}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $addToSet: { }}//TODO: Add 
+                )
+            }
+        }
+    // addCardToDeck
 
-// createDeck
+    // createDeck
 
-// removeCardList
+    // removeCardList
 
-// removeCardDeck
+    // removeCardDeck
+    
+
+    }
+}
+
+
