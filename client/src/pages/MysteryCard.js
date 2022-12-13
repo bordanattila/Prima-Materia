b62de68cf7c797f1ff504c939bc580d281d4e2be
Reed Meher <reed.meher@gmail.com>
2022-12-12 21:30:33 -0600

    mystery card function being built

--- client/src/pages/MysteryCard.js
+++ client/src/pages/MysteryCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Box, Typography, Grid, Button } from "@mui/material";
+import { Box, Typography, Grid, Button, Card, CardActions, CardContent, CardMedia } from "@mui/material";
 
 const MysteryCard = () => {
 
@@ -9,7 +9,7 @@ const MysteryCard = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch(`https://api.magicthegathering.io/v1/cards?pageSize=20;contains=imageUrl;random=true`);
+      const response = await fetch(`https://api.magicthegathering.io/v1/cards?pageSize=1;contains=imageUrl;random=true`);
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -25,9 +25,30 @@ const MysteryCard = () => {
         image: card.imageUrl,
       }));
       setMysteryCard(cardData);
-    }
-
-    catch (err) {
+      return (
+        <Card key={mysteryCard.cardId} sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={mysteryCard.image}
+            alt={mysteryCard.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {mysteryCard.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {mysteryCard.text}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            {/* these buttons need functionality */}
+            <Button size="small">Add to Wishlist</Button>
+            <Button size="small">Add to a Deck:</Button>
+          </CardActions>
+        </Card>
+      );
+    } catch (err) {
       console.error(err);
     }
   };
