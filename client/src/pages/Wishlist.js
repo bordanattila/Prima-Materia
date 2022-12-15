aa71cfc2188c6ef9841109ebc63b9eac42ae0b84
Talee Y <talee.yng@gmail.com>
2022-12-15 11:02:25 -0600

    Add code to display cards on wishlist page

--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,7 +1,66 @@
 import React from "react";
+import Auth from '../utils/auth';
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_CARD_LIST } from "../utils/mutations";
+import {
+  Container,
+  Box
+} from "@mui/material"
+
+import SearchCard from "../components/Card";
 
 const Wishlist = () => {
-  return <h1>Wishlist</h1>;
+  const { loading, data } = useQuery(QUERY_ME);
+
+  const userData = data?.me || {};
+  console.log(userData)
+
+  const [removeCardFromList] = useMutation(REMOVE_CARD_LIST);
+
+  const handleDeleteCardList = async (idCard) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      const { data } = await removeCardFromList({
+        variables: { idCard: idCard }
+      })
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  if (loading) {
+    return <h2>LOADING...</h2>
+  }
+
+  return (
+    <Container maxWidth="md"
+      sx={{margin: "10em"}}>
+      <h3
+        style={{
+          color: "#fff",
+          textAlign: "center"
+        }}>
+        Wishlist</h3>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: { sm: "1fr", md: "1fr 1fr" },
+          gap: 2,
+        }}
+      >
+        {userData.wishList.map((card) => {
+          return <SearchCard card={card} />;
+        })}
+      </Box>
+    </Container>
+
+  )
 };
 
 export default Wishlist;
