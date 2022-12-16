1e17c58a2ab64d11cbb996d3534c22fff1b22cdd
Reed Meher <reed.meher@gmail.com>
2022-12-15 20:04:38 -0600

    database name added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //needs mongodb location
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/prima-materia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/primaDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
