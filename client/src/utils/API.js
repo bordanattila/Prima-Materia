5378315c2fa8afb315ca0847d14ee311b8f0832f
Reed Meher <reed.meher@gmail.com>
2022-12-17 09:53:41 -0600

    supertypes added

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
-export const searchMagicCards = (name, type, subtype, colors) => {
+export const searchMagicCards = (name, type, subtype, supertype, colors) => {
 
-return fetch(`https://api.magicthegathering.io/v1/cards?pageSize=20;contains=imageUrl;random=true;name=${name};types=${type};subtypes=${subtype};colors=${colors}`)
+return fetch(`https://api.magicthegathering.io/v1/cards?pageSize=20;contains=imageUrl;random=true;name=${name};types=${type};supertypes=${supertype};subtypes=${subtype};colors=${colors}`)
 
 }
 
