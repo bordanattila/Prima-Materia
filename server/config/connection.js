fd258a78d404bc756e30e52fb29a4e5c82f637f4
bordanattila <bordan_attila@hotmail.com>
2022-12-18 20:13:37 -0500

    heroku commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //needs mongodb location
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/prima-materia', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prima-materia', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
