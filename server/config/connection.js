7b9a0d0a8a766cd986702ff805f1b700d55f1136
Reed Meher <reed.meher@gmail.com>
2022-12-19 15:45:03 -0600

    fix connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 //needs mongodb location
-<<<<<<< HEAD
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/primaDB', {
-=======
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/prima-materia', {
->>>>>>> cdbf84d46b8c9f189be128162c2a871d7b2af453
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
