8147c42ad60049ba0ecf3feafb607443ca03993a
bordanattila <bordan_attila@hotmail.com>
2022-12-18 20:04:39 -0500

    heroku commit

--- client/src/components/Deck/index.js
+++ client/src/components/Deck/index.js
@@ -8,20 +8,6 @@ import {
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function Decks() {
-// set currentPage state to CurrentDecks to start with
-    // const [currentPage, setCurrentPage] = useState("CurrentDecks");
-
-    //     // render page based on user click
-    //     const renderPage = () => {
-    //         if (currentPage === "CreateDeck") {
-    //             return <CreateDeck />;
-    //         }
-    //         // return <CurrentDecks />
-    //     };
-
-    //     // set current page to the one that is clicked on
-    // const handlePageChange = (page) => setCurrentPage(page);
-
     return (
         <>
     <CurrentDecks />
